3c3c8802f41d8790d395ac769a4ff9683753bd57
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-08-21 01:14:12 +0300

    Refactor add movie code

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,21 @@
 const addForm = document.querySelector('.add');
 const moviesList = document.querySelector('.movies');
 
-addForm.addEventListener('submit', e => {
-  e.preventDefault();
-  const movie = addForm.movie.value.trim();
+const createHtml = (movie) => {
   const html = `
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-      ${movie}
-      <i class="fas fa-trash delete"></i>
-    </li>
-  `
-  moviesList.innerHTML += html;
-  addForm.reset();
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        ${movie}
+        <i class="fas fa-trash delete"></i>
+      </li>
+    `;
+    moviesList.innerHTML += html;
+  }
+  
+  addForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const movie = addForm.movie.value.trim();
+    if(movie){
+      createHtml(movie);
+      addForm.reset();
+    }
 });
